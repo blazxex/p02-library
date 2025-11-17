ac60eb339242b178fbfe9e255d33859d944f9e3f
Blazxex <82823435+blazxex@users.noreply.github.com>
2025-11-17 22:20:22 +0700

    Adjust reservation date validation timezone
    Updated the reservation date validation to use the current date in the Asia/Bangkok timezone.

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -85,7 +85,8 @@ exports.createReservation = async (req, res, next) => {
     }
 
     // Validation: reservation date must not be earlier than today
-    const today = new Date();
+    //const today = new Date();
+    const today = new Date(now.toLocaleString("en-US", { timeZone: "Asia/Bangkok" }));
     today.setHours(0, 0, 0, 0);
     const borrowDate = new Date(req.body.borrowDate);
     const pickupDate = new Date(req.body.pickupDate);
